9bfb693f7bf39c7dd2c6f717595363ee49c4f65c
olevole <olevole@olevole.ru>
2022-05-12 19:36:51 +0300

    fix runtime v1 is deprecated since containerd v1.4, consider using runtime v2

--- kubernetes/install_scripts_secure/install_containerd-v1.6.2.sh
+++ kubernetes/install_scripts_secure/install_containerd-v1.6.2.sh
@@ -20,7 +20,7 @@ cat << EOF | sudo tee /etc/containerd/config.toml
   [plugins.cri.containerd]
     snapshotter = "overlayfs"
     [plugins.cri.containerd.default_runtime]
-      runtime_type = "io.containerd.runtime.v1.linux"
+#      runtime_type = "io.containerd.runtime.v1.linux"
       runtime_engine = "/usr/local/bin/runc"
       runtime_root = ""
 EOF
